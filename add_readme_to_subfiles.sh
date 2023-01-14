9fb036904c429adb08f2aaef63ac7d135d9c925d
0xdcnx <ricardouqueio@gmail.com>
2023-01-14 19:15:39 +0000

    Update add_readme_to_subfiles.sh

--- add_readme_to_subfiles.sh
+++ add_readme_to_subfiles.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#This file simply generates README.md's to all subfolders in this file's path, up to 2 levels of depth
+# This file simply generates README.md's to all subfolders in this file's path, up to 2 levels of depth
 # This is so I can add my empty subfolders in git, which does not allow empty folders, without manually adding READMEs :(
 
-#Example: Let's say this file is located under /folder/ which has the following folder structure:
-#/folder/
+# Example: Let's say this file is located under /folder/ which has the following folder structure:
+# /folder/
 
-#/folder/aaa/folderA
-#/folder/aaa/folderB
+# /folder/aaa/folderA
+# /folder/aaa/folderB
 
-#/folder/bbb/fileC
-#/folder/bbb/fileD
+# /folder/bbb/fileC
+# /folder/bbb/fileD
 
 # A README.md is then created in /folder/aaa, /folder/aaa/folderA, /folder/aaa/folderB, /folder/bbb/ ... and so on
 
@@ -18,28 +18,29 @@
 #PS: There should be a better way to do this, this is just what I came up with
 #=======================
 
-BASE="/home/rich/ctf_writeups"
+
+BASE="/path/to/git_root"
 current_dir=${PWD##*/}
 
-#The first iteration goes thru all folders in depth level 1
+# The first iteration goes through all folders in depth level 1
 for d in $BASE/*; do
   if [ -d "$d" ]; then
 
-    #I use cd so that afterwards I can use PWD to get the current dir as a variable
+    # I use cd so that afterwards I can use PWD to get the current dir as a variable
     cd "$d"
     current_dir=${PWD##*/} # gets current depth 1 dir as a variable
     
 
-    #echoes the initial data to a README.md files in depth 1
+    # echoes the initial data to a README.md files in depth 1
     echo "Writeups for $current_dir challenges" > "$d/README.md"
     
-    #The second iteration goes thru all folders in depth level 2
+    # The second iteration goes thru all folders in depth level 2
     for dd in $d/*; do
     	cd "$dd"
-    	current_dir=${PWD##*/} #gets current depth level 2 dir as a variable
+    	current_dir=${PWD##*/} # gets current depth level 2 dir as a variable
     	if [ -d "$dd" ]; then
    		
-   		 #echoes the initial data to a README.md files in depth 2
+   		 # echoes the initial data to a README.md files in depth 2
     		echo "Writeups for $current_dir challenges" > README.md
     	fi
     done
